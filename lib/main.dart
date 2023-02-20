059702e31ef2d8b5c06315c86813e113fe5b8678
Charan Teja <68527259+itscharanteja@users.noreply.github.com>
2023-02-20 22:32:48 +0530

    flutter files

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+
 class TimerApp extends StatefulWidget {
   @override
   // ignore: library_private_types_in_public_api
   _TimerAppState createState() => _TimerAppState();
 }
 
-class _TimerAppState {
-}
+class _TimerAppState {}
